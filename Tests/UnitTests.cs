3290f5d5c6b102d96a1c8ccb84d15be628de924d
Nick Nguyen <lnickdnguyen@gmail.com>
2019-03-28 20:13:01 -0500

    Fixed tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -60,8 +60,8 @@ namespace TravisCILab
         public void Multiply_Valid()
         {
             Assert.AreEqual(2, Program.Multiply("1", "2"));
-            Assert.AreEqual(6, Program.Add("3", "2"));
-            Assert.AreEqual(35, Program.Add("5", "7"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
         }
 
         [Test]
@@ -107,7 +107,7 @@ namespace TravisCILab
         [Test]
         public void Power_Valid()
         {
-            Assert.AreEqual(2, Program.Power("1", "2"));
+            Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
             Assert.AreEqual(5, Program.Power("5", "1"));
         }
